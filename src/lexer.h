d1b2df1fd9cbd0b7e22bd504581fe0922cf37e92
CheEmeT <mnmn2220@gmail.com>
2022-07-18 05:04:34 +0300

    Added a few files

--- src/lexer.h
+++ src/lexer.h
@@ -1,9 +1,28 @@
-//
-// Created by Admin on 16.07.2022.
-//
 
 #ifndef CHEEMTON_LEXER_H
 #define CHEEMTON_LEXER_H
+
+#include <string>
+
+enum TokenType{
+    NUMBER,
+    PARANTHES_OPEN,
+    PARANTHES_CLOSED,
+};
+
+class Token{
+private:
+    TokenType type_;
+    std::string data_;
+public:
+    Token(TokenType type, std::string data);
+    Token(TokenType type);
+
+    TokenType getType(){ return type_; }
+
+    std::string getData(){ return data_ ;
+    }
+};
 
 class Lexer{
 
