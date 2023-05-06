be44221459b92e155289a16c1fce18f395937702
Cheemet <mnmn2220+git@gmail.com>
2023-05-06 17:58:09 +0300

    Started Again

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
-#include "lexer.h"
-#include "parser.h"
-
-int main() {
-    std::string code = "(2 + 1) * 3 - 3\n";
-    cheemton::Lexer lexer(code);
-    auto v = lexer.getTokens();
-    for(auto & ref : v){
-        std::cout << *ref << std::endl;
-    }
-    //cheemton::Parser parser(std::move(v));
-    //std::cout << *parser.getTokenTree();
 
 
 
-    return 0;
+int main(int argc, char** argv) {
+	std::cout << "Starting program\n";
 }
